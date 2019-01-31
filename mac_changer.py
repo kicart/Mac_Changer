f5ef422c7bfd07500345c84f7fa75d57ff1de65a
root <root@localhost.localdomain>
2019-01-30 20:25:37 -0500

    first commit for mac_changer

--- mac_changer.py
+++ mac_changer.py
@@ -5,13 +5,15 @@ import optparse
 import re
 
 
-
 def get_arguments():
+    #create a parser to collect the option given by the user, looking for the interface ("eth0, etc") and the requested
+    #MAC
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
 
+    #if the user did not add options, provide these statements asking them to specify
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
@@ -20,12 +22,15 @@ def get_arguments():
 
 
 def change_mac(interface, new_mac) :
+    #shuts down our interface variable so we can change it, and then brings it back up.
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 def get_current_mac(interface):
+    #calls ifconfig for the interface we provided, and then search for alphanumeric characters in the pattern of a MAC
+    #address, if one is found return it. If not, print a statement saying so
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
